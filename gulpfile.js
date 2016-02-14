1a4a0dcb02a6b3e72f979f6fcd7d9a9f519118a9
jbreckmckye <jimmy@breck-mckye.com>
2016-02-14 16:58:25 +0000

    Can bundle JS

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 const gulp = require('gulp');
 
+const browserify = require('browserify');
 const clean = require('gulp-clean');
 const concat = require('gulp-concat');
 const inject = require('gulp-inject');
 const webserver = require('gulp-webserver');
+const vinylSource = require('vinyl-source-stream');
 
 gulp.task('clean', ()=> {
     const buildDir = gulp.src('./build/*', {read: false});
@@ -11,12 +13,13 @@ gulp.task('clean', ()=> {
 });
 
 gulp.task('deploy', ()=> {
-    const appJS = gulp.src(['./src/js/*.js'])
-        .pipe(concat('app.js'))
+    const jsBundle = browserify('./src/js/index.js')
+        .bundle()
+        .pipe(vinylSource('bundle.js'))
         .pipe(gulp.dest('./build'));
 
     gulp.src('./src/index.html')
-        .pipe(inject(appJS, {ignorePath: 'build'}))
+        .pipe(inject(jsBundle, {ignorePath: 'build'}))
         .pipe(gulp.dest('./build'));
 });
 
